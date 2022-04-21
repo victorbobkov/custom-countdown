d65a98efdc26b63cebcd95d79959eef3f487af45
victor bobkov <victorbobkov@gmail.com>
2022-04-21 23:41:56 +0400

    Store Form values

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const dateElement = document.getElementById('date-picker')
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0]
 dateElement.setAttribute('min', today)
+
+let countdownTitle = ''
+let countdownDate = ''
+
+// Event Listeners
+countdownForm.addEventListener('submit', (event) => {
+   // Take Values from Form Input
+   event.preventDefault()
+   countdownTitle = event.srcElement[0].value
+   countdownDate = event.srcElement[1].value
+   console.log(countdownTitle, countdownDate)
+})
